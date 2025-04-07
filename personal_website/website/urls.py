fc2156eb40d85e59c1c9742c9d9acfabc2621a45
cjjohanson <46818843+cjjohanson@users.noreply.github.com>
2025-04-07 07:31:25 -0400

    delete django based blog to be replace with tailwind react static site

--- personal_website/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from website.views import HomeView, BlogView, ResumeView, ContactUsView
-
-urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("blog", BlogView.as_view(), name="blog"),
-    path("resume", ResumeView.as_view(), name="resume"),
-    path("contact", ContactUsView.as_view(), name="contact"),
-]
